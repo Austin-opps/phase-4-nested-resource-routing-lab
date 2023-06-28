b452dd9bc9a2639d30335e0e46b9a1c3d808faa7
Austin <akomen04@gmail.com>
2023-06-28 08:47:31 +0300

    Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
-
   def show
     user = User.find_by(id: params[:id])
-    render json: user, include: :items
+    if user
+      render json: user, include: :items
+    else
+      render json: { error: "User not found" }, status: :not_found
+    end
   end
 
+  # def index
+  #   users = User.find(id: params[:id])
+  #   if users
+  #     render json: users, include: :items
+  #   else
+  #     render json: { error: "User not found" }, status: :not_found
+  #   end
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :items do
+    resources :magazines, only: [:index]
+  end
+  resources :users do
+    resources :items, only: [:index, :show, :create]
+  end
 end
